4b0c783a7b844ba617d103dd04f65da9067092c7
Villefor <franklincrius@gmail.com>
2021-12-22 15:31:18 -0300

    ajustes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dddd31b485240598"; }
+/******/ 		__webpack_require__.h = function() { return "5c2a7c499d6ebc5d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
