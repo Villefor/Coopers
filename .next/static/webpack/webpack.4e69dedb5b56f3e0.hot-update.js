4102740142d8b831fd7246e9f3fc9d5a31f43ebf
Villefor <franklincrius@gmail.com>
2021-12-22 20:40:24 -0300

    ajustes de width

--- .next/static/webpack/webpack.4e69dedb5b56f3e0.hot-update.js
+++ .next/static/webpack/webpack.4e69dedb5b56f3e0.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ed73d95e7cf67f0c"; }
+/******/ 	__webpack_require__.h = function() { return "f124b55a08421347"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
