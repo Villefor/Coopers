d864f9037bea832f91854ddb683453db8caf52a0
Villefor <franklincrius@gmail.com>
2021-12-23 01:12:59 -0300

    ajustes do header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "511b9dccdab5c9b5"; }
+/******/ 		__webpack_require__.h = function() { return "5e9495b100aba44d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.4e69dedb5b56f3e0.hot-update.js
+++ .next/static/webpack/webpack.4e69dedb5b56f3e0.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "f124b55a08421347"; }
+/******/ 	__webpack_require__.h = function() { return "4d7154cccc2783dd"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
