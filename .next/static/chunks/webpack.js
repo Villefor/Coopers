c632ef3c9e4aa4fcc8a9727ca839e634ab203fb8
Villefor <franklincrius@gmail.com>
2021-12-23 01:42:14 -0300

    ajustes para o deploy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e9495b100aba44d"; }
+/******/ 		__webpack_require__.h = function() { return "1083731e8fec9fb7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
