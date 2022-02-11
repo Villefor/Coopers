111afbb2ad581588c618029d0e7d74a53cb22bcc
Villefor <franklincrius@gmail.com>
2022-02-11 01:30:31 -0300

    ajustes

--- web/components/Footer/index.js
+++ web/components/Footer/index.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react'
 import styles from './styles.module.scss'
 
 const Footer = (props) => {
-  console.log(props)
   return (
     <section className={styles.section}>
       <footer className={styles.footer}>
