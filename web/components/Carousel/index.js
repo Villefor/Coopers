10f1655fa24c7b1cb7aea369a93ad6266c0c842f
Villefor <franklincrius@gmail.com>
2022-02-23 15:14:53 -0300

    [feat]-ajustes pautados no feedbac, implementaçao do swiper

--- web/components/Carousel/index.js
+++ web/components/Carousel/index.js
@@ -1,32 +1,21 @@
 import React, { useRef, useState, useEffect } from 'react'
 import styles from './styles.module.scss'
-// import 'swiper/css'
-// import 'swiper/css/pagination'
-import Carousel from 'react-multi-carousel'
-import 'react-multi-carousel/lib/styles.css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { useMediaQuery } from '../../hooks/useMediaQuery'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/scrollbar'
 
 function Carousels(props) {
   const [images, setImages] = useState([])
+  const isMobile = useMediaQuery(850)
+  const isTablet = useMediaQuery(1023)
 
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 765 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 765, min: 0 },
-      items: 1,
-    },
-  }
+  useEffect(() => {
+    console.log(isMobile)
+  }, [isMobile, isTablet])
 
   useEffect(() => {
     if (images.length === 0) {
@@ -45,48 +34,45 @@ function Carousels(props) {
       <div className={styles.carouselSection_Background}>
         <p className={styles.carouselSection_Paragraph}>good things</p>
       </div>
-      {/* <div className={styles.carrouselCardContainer}> */}
-      <Carousel
-        showDots
-        focusOnSelect={true}
-        infinite={true}
-        slidesToSlide={3}
-        arrows={false}
-        swipeable={true}
-        draggable={false}
-        showDots={true}
-        responsive={responsive}
-        ssr={true} // means to render carousel on server-side.
-        infinite={true}
-        keyBoardControl={true}
-        customTransition='all .5'
-        containerClass={styles.carrouselCardContainer}
-        removeArrowOnDeviceType={['tablet', 'mobile']}
-        dotListClass={'custom-dot-list-style'}
-        itemClass='carousel-item-padding-40-px'
-      >
-        {images !== 0
-          ? images.map((item, index) => {
-              return (
-                <div key={index} className={styles.carrouselCardContent}>
-                  <div className={styles.carroselHeaderImg}>
-                    <img key={index} src={item.img} />
-                  </div>
-                  <div className={styles.carroselHeaderLogo}>
-                    <img src='/images/icon_coopers.png' />
-                  </div>
-                  <div className={styles.carroselDescription}>
-                    <button>function</button>
-                    <p key={index}> {item.description} </p>
-                    <p className={styles.readMoreText}>read more</p>
-                  </div>
-                </div>
-              )
-            })
-          : null}
-      </Carousel>
-      {/* </div> */}
-      <div className={styles.buttonsCarrousel}></div>
+      <div className={styles.carrouselCardContainer}>
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView='auto'
+          spaceBetween={30}
+          slidesPerGroup={isMobile ? 1 : isTablet ? 2 : 3}
+          // loop={true}
+          // loopFillGroupWithBlank={true}
+          pagination={{
+            dynamicBullets: true,
+            clickable: true,
+          }}
+        >
+          {images !== 0
+            ? images.map((item, index) => {
+                return (
+                  <SwiperSlide key={index} style={{ maxWidth: '355px' }}>
+                    <div key={index} className={styles.carrouselCardContent}>
+                      <div className={styles.carroselHeaderImg}>
+                        <img key={index} src={item.img} alt='Carousel Images' />
+                      </div>
+                      <div className={styles.carroselHeaderLogo}>
+                        <img
+                          src='/images/icon_coopers.png'
+                          alt='Coopers Company icon'
+                        />
+                      </div>
+                      <div className={styles.carroselDescription}>
+                        <button>function</button>
+                        <p key={index}> {item.description} </p>
+                        <p className={styles.readMoreText}>read more</p>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                )
+              })
+            : null}
+        </Swiper>
+      </div>
     </section>
   )
 }
